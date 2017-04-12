5d6feae4f52dda550de42dbe8049c5e871210b93
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2017-04-12 01:08:55 -0300

    Initial commit.

---
+++ init.el
@@ -0,0 +1,35 @@
+
+;; package.el config.
+
+(require 'package)
+(package-initialize)
+
+					; Here we define the repos to be used.
+(setq package-archives
+      '(("melpa-stable" . "http://stable.melpa.org/packages/")
+	("gnu"          . "https://elpa.gnu.org/packages/")))
+
+					; Download a copy of the package
+					; archive (only for the first time).
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+;; use-package for managing dependencies.
+
+					; Install `use-package` (if not
+					; present)
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(require 'use-package)
+
+					; Set ":ensure t" for every required
+					; package.
+(setq use-package-always-ensure t)
+
+;; Configuration modules
+
+(add-to-list 'load-path "~/.emacs.d/config/")
+(add-to-list 'load-path "~/.emacs.d/config/lang")
+
+
