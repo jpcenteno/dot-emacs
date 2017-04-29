64082526ac44801eb6b577aefb4bac2282714a3c
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2017-04-29 00:08:35 -0300

    Add aggressive indent mode

--- config/editing.el
+++ config/editing.el
@@ -3,3 +3,7 @@
 (use-package flycheck
   :init
   (global-flycheck-mode))
+
+(use-package aggressive-indent-mode
+  (global-aggressive-indent-mode 1)
+  (add-to-list 'aggressive-indent-excluded-modes 'haskell-mode))
