1fe311efe8789a859885633540f149455e016547
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2018-08-06 17:24:37 -0300

    remove the function that loads every elisp file in dir

--- init.el
+++ init.el
@@ -27,31 +27,3 @@
 					; package.
 (setq use-package-always-ensure t)
 
-;; Configuration modules
-(defun load-elisp-files-in-dir (DIRPATH)
-  "Loads every emacs lisp file in some DIRPATH directory."
-
-  ;; Add `DIRPATH` to the load path if it is not already there.
-  (unless (member DIRPATH load-path)
-    (add-to-list 'load-path DIRPATH))
-
-  (let (;; Paths of the elisp files in the dir.
-	(files-to-load (directory-files DIRPATH
-					t ; return absolute paths.
-					".+\\.elc?$")))
-    (dolist (file files-to-load)
-      
-      (unless (member file ; if the file is not already loaded.
-		      ;; List of files that are already loaded:
-		      (mapcar #'car load-history))
-	
-					; load it
-	(load (file-name-sans-extension
-	       (file-name-nondirectory
-		file)))))))
-
-
-;; Load the modules in those dirs. To omit files, change their extension or
-;; remove them.
-(load-elisp-files-in-dir "~/.emacs.d/config/")
-(load-elisp-files-in-dir "~/.emacs.d/config/lang")
