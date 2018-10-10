2cff7169d1e8390c6ca90b2485be9405fa969682
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2018-10-10 01:08:34 -0300

    org-mode homogeneous font size

--- init.el
+++ init.el
@@ -48,6 +48,17 @@
   :config
   (setq helm-ag-base-command "rg --nocolor --nogroup --ignore-case"))
 
+;; org-mode ------------------------------------------------------------------
+
+;; Keep the same size on different files.
+(custom-set-faces
+  '(org-document-title ((t (:inherit org-document-title :height 1.0))))
+  '(org-level-1        ((t (:inherit outline-1          :height 1.0))))
+  '(org-level-2        ((t (:inherit outline-2          :height 1.0))))
+  '(org-level-3        ((t (:inherit outline-3          :height 1.0))))
+  '(org-level-4        ((t (:inherit outline-4          :height 1.0))))
+  '(org-level-5        ((t (:inherit outline-5          :height 1.0)))))
+
 ;; Mac Os --------------------------------------------------------------------
 
 (if (string-equal system-type "darwin")
